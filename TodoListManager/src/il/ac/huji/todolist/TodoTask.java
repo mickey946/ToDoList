eca0d8c6b6d67fb203c01eebcf8400a9e993be83
Mickey Shnaiderman <mickey946@gmail.com>
2014-03-28 17:35:45 +0300

    Representing a task using a class instead of a pair

---
+++ TodoListManager/src/il/ac/huji/todolist/TodoTask.java
@@ -0,0 +1,41 @@
+package il.ac.huji.todolist;
+
+public class TodoTask {
+	
+	private int _id;
+	private String _title;
+	private long _due;
+	
+	public TodoTask() {
+	}
+
+	public TodoTask(int id, String title, long due) {
+		this._id = id;
+		this._title = title;
+		this._due = due;
+	}
+
+	public int getID() {
+		return _id;
+	}
+
+	public void setID(int id) {
+		this._id = id;
+	}
+
+	public String getTitle() {
+		return _title;
+	}
+
+	public void setTitle(String title) {
+		this._title = title;
+	}
+
+	public long getDue() {
+		return _due;
+	}
+
+	public void setDue(long due) {
+		this._due = due;
+	}
+}
